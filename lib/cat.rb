d0862132c1b5714708a13f67ba63f07b6bbbe7c7
Karl Benbeneck <kbenbeneck@gmail.com>
2020-02-26 02:19:30 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,11 @@
 class Cat
-  attr_accessor :name, :owner
+  attr_accessor :owner, :mood
+  attr_reader :name
   @@all = []
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
     @@all << self
   end
 
