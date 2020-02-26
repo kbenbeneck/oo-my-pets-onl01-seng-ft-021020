55635db384e5d8cddc7c734d80361901f869dd0d
Karl Benbeneck <kbenbeneck@gmail.com>
2020-02-26 02:43:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,4 +41,12 @@ class Owner
   def buy_dog(name)
     Dog.new(name, self)
   end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
 end
